f112cf90e37e28b45dc6ffa1b6ba98aabbf1d027
Sergio Uve <sergiouve@gmail.com>
2017-12-18 21:20:08 +0100

    Add new gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,35 +5,31 @@ const browserify  = require('browserify');
 const sass        = require('gulp-sass');
 const source      = require('vinyl-source-stream');
 const shell       = require('gulp-shell');
+const babelify    = require('babelify');
 
 gulp.task('browserify', function() {
-    return browserify('./src/js/app.source.js')
+    return browserify('./src/js/app.js')
+        .transform(babelify, {presets: ["es2015"]})
         .bundle()
         .pipe(source('app.js'))
         .pipe(gulp.dest('./assets/js/'));
 });
 
 gulp.task('sass', function() {
-    gulp.src('./src/scss/main.scss')
+    gulp.src('./src/scss/app.scss')
         .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(gulp.dest('./assets/css'));
 });
 
-gulp.task('js', function() {
-    gulp.src('./src/js/startx/startx.js')
-        // .pipe(uglify())
-        .pipe(gulp.dest('assets/js/'));
-});
-
 gulp.task('serve', shell.task([
-  'php -S localhost:3000'
+  'http-server ./ -p 3000'
 ]));
 
 gulp.task('watch', function() {
-    gulp.watch('./src/js/*.js', ['browserify']);
-    gulp.watch('./src/js/startx/*.js', ['js']);
     gulp.watch('./src/js/*/*.js', ['browserify']);
+    gulp.watch('./src/js/*.js', ['browserify']);
     gulp.watch('./src/scss/*/*.scss', ['sass']);
+    gulp.watch('./src/scss/*.scss', ['sass']);
 });
 
-gulp.task('default', ['browserify', 'sass', 'js', 'serve', 'watch']);
+gulp.task('default', ['browserify', 'sass', 'serve', 'watch']);
