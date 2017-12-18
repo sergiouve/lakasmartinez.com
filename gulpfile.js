6ebd2dca62b72cd903183a456b8dd99ce8ade4ad
Sergio Uve <sergiouve@gmail.com>
2017-12-18 21:12:01 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+const gulp        = require('gulp');
+const gutil       = require('gulp-util');
+const uglify      = require('gulp-uglify');
+const browserify  = require('browserify');
+const sass        = require('gulp-sass');
+const source      = require('vinyl-source-stream');
+const shell       = require('gulp-shell');
+
+gulp.task('browserify', function() {
+    return browserify('./src/js/app.source.js')
+        .bundle()
+        .pipe(source('app.js'))
+        .pipe(gulp.dest('./assets/js/'));
+});
+
+gulp.task('sass', function() {
+    gulp.src('./src/scss/main.scss')
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(gulp.dest('./assets/css'));
+});
+
+gulp.task('js', function() {
+    gulp.src('./src/js/startx/startx.js')
+        // .pipe(uglify())
+        .pipe(gulp.dest('assets/js/'));
+});
+
+gulp.task('serve', shell.task([
+  'php -S localhost:3000'
+]));
+
+gulp.task('watch', function() {
+    gulp.watch('./src/js/*.js', ['browserify']);
+    gulp.watch('./src/js/startx/*.js', ['js']);
+    gulp.watch('./src/js/*/*.js', ['browserify']);
+    gulp.watch('./src/scss/*/*.scss', ['sass']);
+});
+
+gulp.task('default', ['browserify', 'sass', 'js', 'serve', 'watch']);
